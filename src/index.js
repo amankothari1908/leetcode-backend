125addc41fe79c12b0424a19757fe9381a2bf7ea
Aman <aman@Amans-MacBook-Air.local>
2025-04-25 17:24:16 +0530

    added routinh structure for version v1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("../src/config/server.config");
+const apiRouter = require("./routes");
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use(bodyParser.text());
 app.get("/ping", (req, res) => {
   return res.json({ message: "Leetcode problem service is live" });
 });
+
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server Started at PORT: ${PORT}`);
